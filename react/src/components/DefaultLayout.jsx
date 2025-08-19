f4ad66159ab9b6b82a1447d66f2184479826a3ca
Albina Ahmeti <59178186+AlbinaAhmeti@users.noreply.github.com>
2025-08-19 17:49:56 +0200

    Implement Notification

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,29 +1,29 @@
-import { Link, Navigate, Outlet } from "react-router-dom";
-import { useStateContext } from "../context/ContextProvider";
-import { useEffect } from "react";
-import axiosClient from "../axios-client";
+import {Link, Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../context/ContextProvider";
+import axiosClient from "../axios-client.js";
+import {useEffect} from "react";
 
 export default function DefaultLayout() {
-  const { user, token, setUser, setToken } = useStateContext();
+  const {user, token, setUser, setToken, notification} = useStateContext();
 
   if (!token) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login"/>
   }
 
-  const onLogout = (ev) => {
-    ev.preventDefault();
+  const onLogout = ev => {
+    ev.preventDefault()
 
     axiosClient.post('/logout')
       .then(() => {
         setUser({})
         setToken(null)
       })
-  };
+  }
 
   useEffect(() => {
     axiosClient.get('/user')
       .then(({data}) => {
-        setUser(data)
+         setUser(data)
       })
   }, [])
 
@@ -35,18 +35,24 @@ export default function DefaultLayout() {
       </aside>
       <div className="content">
         <header>
-          <div>Header</div>
           <div>
-            {user.name}
-            <a href="#" onClick={onLogout} className="btn-logout">
-              Logout
-            </a>
+            Header
+          </div>
+
+          <div>
+            {user.name} &nbsp; &nbsp;
+            <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
           </div>
         </header>
         <main>
-          <Outlet />
+          <Outlet/>
         </main>
+        {notification &&
+          <div className="notification">
+            {notification}
+          </div>
+        }
       </div>
     </div>
-  );
+  )
 }
